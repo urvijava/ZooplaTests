2f3433bdf402ce6fe51f75e6fb1f97117d22096d
urvijava <123264086+urvijava@users.noreply.github.com>
2023-01-27 19:29:36 +0000

    intial commit

---
+++ src/test/java/org/example/zooplaTests/selenium/DummyClassTest.java
@@ -0,0 +1,10 @@
+package org.example.zooplaTests.selenium;
+
+import org.junit.Test;
+
+public class DummyClassTest {
+    @Test
+    public void simpleSearch() {
+        System.out.println("my first test");
+    }
+}
