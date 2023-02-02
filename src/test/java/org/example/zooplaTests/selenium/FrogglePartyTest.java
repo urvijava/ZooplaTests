488483ba348775bd8e6b35cdcc5071569276e51f
urvijava <123264086+urvijava@users.noreply.github.com>
2023-02-02 20:20:14 +0000

    partytab test

--- src/test/java/org/example/zooplaTests/selenium/FrogglePartyTest.java
+++ src/test/java/org/example/zooplaTests/selenium/FrogglePartyTest.java
@@ -2,6 +2,7 @@ package org.example.zooplaTests.selenium;
 
 import org.junit.Assert;
 import org.junit.Before;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -20,6 +21,19 @@ public class FrogglePartyTest {
         String title = "Froggle Parties | Children’s Party Entertainers | Kids Parties";
         Assert.assertEquals(title,driver.getTitle());
 
+        driver.quit();
+
+    }
+
+    @org.junit.Test
+    public void frogglePartyTabTest() {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.froggleparties.com/");
+        driver.findElement(By.id("menu-item-2321")).click();
+        driver.findElement(By.xpath("//a[@href='https://www.froggleparties.com/parties/entertainers-magicians']")).click();
+        String bigTitle = driver.findElement(By.xpath("//div[@data-id='de24b5e']")).getText();
+        String expectedTitle = "Children's party entertainers and magicians";
+        Assert.assertEquals(expectedTitle,bigTitle);
         driver.quit();
 
     }
